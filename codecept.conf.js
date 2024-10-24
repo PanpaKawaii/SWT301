69051be2244c82d7e99157ef74ee950638586806
PanpaKawaii <dngngchitriu2004@gmail.com>
2024-10-24 22:47:21 +0700

    AI Test

--- codecept.conf.js
+++ codecept.conf.js
@@ -1,49 +1,49 @@
-const { setHeadlessWhen, setCommonPlugins } = require("@codeceptjs/configure");
-// turn on headless mode when running with HEADLESS=true environment variable
-// export HEADLESS=true && npx codeceptjs run
-setHeadlessWhen(process.env.HEADLESS);
-require("dotenv").config();
-require("./heal");
-// enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
-setCommonPlugins();
+// const { setHeadlessWhen, setCommonPlugins } = require("@codeceptjs/configure");
+// // turn on headless mode when running with HEADLESS=true environment variable
+// // export HEADLESS=true && npx codeceptjs run
+// setHeadlessWhen(process.env.HEADLESS);
+// require("dotenv").config();
+// require("./heal");
+// // enable all common plugins https://github.com/codeceptjs/configure#setcommonplugins
+// setCommonPlugins();
 
-/** @type {CodeceptJS.MainConfig} */
-exports.config = {
-  tests: "./*_test.js",
-  output: "./output",
-  helpers: {
-    Playwright: {
-      browser: "chromium",
-      url: "https://kit-stem-hub-fe-customer.vercel.app",
+// /** @type {CodeceptJS.MainConfig} */
+// exports.config = {
+//   tests: "./*_test.js",
+//   output: "./output",
+//   helpers: {
+//     Playwright: {
+//       browser: "chromium",
+//       url: "https://kit-stem-hub-fe-customer.vercel.app",
 
-      show: true,
-    },
-  },
-  include: {
-    I: "./steps_file.js",
-  },
-  name: "ai_test",
-  // ai: {
-  //   request: async (messages) => {
-  //     const OpenAI = require("openai");
-  //     const openai = new OpenAI({ apiKey: process.env["OPENAI_API_KEY"] });
+//       show: true,
+//     },
+//   },
+//   include: {
+//     I: "./steps_file.js",
+//   },
+//   name: "ai_test",
+//   ai: {
+//     request: async (messages) => {
+//       const OpenAI = require("openai");
+//       const openai = new OpenAI({ apiKey: process.env["OPENAI_API_KEY"] });
 
-  //     const completion = await openai.chat.completions.create({
-  //       model: "gpt-3.5-turbo-0125",
-  //       messages,
-  //     });
+//       const completion = await openai.chat.completions.create({
+//         model: "gpt-3.5-turbo-0125",
+//         messages,
+//       });
 
-  //     return completion?.choices[0]?.message?.content;
-  //   },
-  // },
-  plugins: {
-    heal: {
-      enabled: true,
-    },
-    allure: {
-      enabled: true,
-      require: "@codeceptjs/allure-legacy",
-      outputDir: "./allure-results",
-    },
-  },
-};
+//       return completion?.choices[0]?.message?.content;
+//     },
+//   },
+//   plugins: {
+//     heal: {
+//       enabled: true,
+//     },
+//     allure: {
+//       enabled: true,
+//       require: "@codeceptjs/allure-legacy",
+//       outputDir: "./allure-results",
+//     },
+//   },
+// };
